f1d91706b06feaf187937072f5d89fe7e7267133
Ni He <ni.he@qq.com>
2018-05-14 14:48:28 +0800

    update

--- code/WeChat/joke.py
+++ code/WeChat/joke.py
@@ -1,36 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May  6 15:44:17 2018
-
-@author: Ni He
+@author Ni He
 """
 
 import itchat, random
-#import pandas as pd
+import time
 
 
-def find_friend(nick_name):
+def find_friend(RemarkName):
   for friend in itchat.get_friends():
-    if friend['RemarkName'] == nick_name:
+    if friend['RemarkName'] == RemarkName:
       return friend
 
 
 itchat.auto_login(True)
-friendID = find_friend('小老虎')
-SINCERE_WISH = [u'去你的',u'滚',u'想死呀',u'见鬼吧~',u'考试挂科咯!']
-#SINCERE_WISH = [u'祝中秋快乐',u'中秋快乐呀',u'中秋快乐哟',u'中秋节快乐呀~',u'中秋节快乐!',u'中秋国庆快乐!']
-for i in range(0,10):  
-        SEND_WISH=random.choice(SINCERE_WISH)
-        itchat.send_msg(SEND_WISH,toUserName=friendID['UserName'])
+friendID = find_friend('叶珍珍')
+wish = [u'去你的',u'滚',u'想死呀',u'考试挂科咯!']
 
-@itchat.msg_register(itchat.content.TEXT)   #这里的TEXT表示如果有人发送文本消息，那么就会调用下面的方法
-def simple_reply(msg, friendID):
-    #这个是向发送者发送消息
-    for i in range(0,10):  
-        SEND_WISH=random.choice(SINCERE_WISH)
-        if msg['FromUserName'] == friendID['UserName']:
-            itchat.send_msg(SEND_WISH,toUserName=friendID['UserName'])
-    return     
+no = 0
+
+while no < 20: # 骚扰20次
+    no = no + 1
+    #if time.time() - start_time > 10: #  每30秒发一次，可以改时间间隔
+        #start_time = time.time()
+    a_wish=random.choice(wish)
+    itchat.send_msg(a_wish,toUserName=friendID['UserName'])
+    time.sleep(10) # 暂停10秒
+
+
 
 
 
